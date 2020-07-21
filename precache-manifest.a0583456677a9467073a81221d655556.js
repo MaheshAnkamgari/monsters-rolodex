32a4b10d3d11d6d83ba5d55f1d8d9e072454c554
Mahesh Ankamgari <am00623707@techmahindra.com>
2020-07-21 11:33:38 +0530

    Updates

---
+++ precache-manifest.a0583456677a9467073a81221d655556.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a30d3979fdc11e8fa7a1cccdd8ad00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b7768cfffce7916cf1b",
+    "url": "/monsters-rolodex/static/css/main.f68cbbf8.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monsters-rolodex/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "4b7768cfffce7916cf1b",
+    "url": "/monsters-rolodex/static/js/main.611ddd48.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
